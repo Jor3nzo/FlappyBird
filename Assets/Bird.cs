98c9297951fe41d2e494a5673ba0bbe66da8c1ab
ZanY <135144670+Jor3nzo@users.noreply.github.com>
2023-10-11 19:51:29 +0300

    Jumping and scoring

---
+++ Assets/Bird.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Bird : MonoBehaviour
+{
+    public int score;
+    public float jumpSpeed;
+    Rigidbody2D rb;
+    public float rotateScale;
+
+
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();  
+    }
+
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            rb.velocity = Vector2.up * jumpSpeed;
+        }
+
+
+        transform.eulerAngles = new Vector3(0,0, rb.velocity.y * rotateScale);
+    }
+
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        Die();
+    }
+
+    void Die()
+    {
+        var currentScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        score++;
+    }
+
+}
---
+++ Assets/Pipe.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pipe : MonoBehaviour
+{
+    public float speed;
+    public float screenStartX;
+    public float screenEndX;
+
+    private void Start()
+    {
+        screenStartX = Camera.main.ViewportToWorldPoint(new Vector3(0f, 0.5f)).x - 1;
+        screenEndX = Camera.main.ViewportToWorldPoint(new Vector3(1f, 0.5f)).x + 1;
+    }
+    
+    private void Update()
+    {
+        transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (transform.position.x < screenStartX)
+        {
+            var height = Random.Range(-2, 4);
+
+            transform.position = new Vector3(screenEndX,height);
+        }
+    }
+
+    
+}
